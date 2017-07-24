3857d8a3e0f410decd5e7a5365856b1edbaa42f9
Michael Clark <michaeljclark@mac.com>
2017-07-24 19:44:24 +1200

    Gather additional uop stats on Intel x86

--- rv8-bench.js
+++ rv8-bench.js
@@ -17,20 +17,39 @@ function bench_cmd(bench, type, cmd, args)
   if (obj.status == 0) {
     var append = "";
     var dmips = obj.stdout.toString().match(/\s([0-9]+)\sDMIPS/);
+    //dhrystone mips
     if (dmips) {
       append += ' | dmips=' + dmips[1];
     }
+    // riscv instructions retired
     var rv_inst = obj.stdout.toString().match(/instret\s+:\s+([0-9]+)/m);
     if (rv_inst) {
       append += ' | rv_inst=' + rv_inst[1];
     }
+    // x86 instructions retired
     var x86_inst = obj.stderr.toString().match(/([0-9,]+)\s+instructions/m);
     if (x86_inst) {
       append += ' | x86_inst=' + x86_inst[1].split(',').join('');
     }
-    var x86_cycles = obj.stderr.toString().match(/([0-9,]+)\s+r1c2/m);
-    if (x86_cycles) {
-      append += ' | x86_cycles=' + x86_cycles[1].split(',').join('');
+    // unfused domain, uops sent to execution port
+    var x86_uops_executed = obj.stderr.toString().match(/([0-9,]+)\s+r1b1/m);
+    if (x86_uops_executed) {
+      append += ' | x86_uops_executed=' + x86_uops_executed[1].split(',').join('');
+    }
+    // fused domain, uops issued
+    var x86_uops_issued = obj.stderr.toString().match(/([0-9,]+)\s+r10e/m);
+    if (x86_uops_issued) {
+      append += ' | x86_uops_issued=' + x86_uops_issued[1].split(',').join('');
+    }
+    // fused domain, uop retirement slots used
+    var x86_uops_retired_slots = obj.stderr.toString().match(/([0-9,]+)\s+r2c2/m);
+    if (x86_uops_retired_slots) {
+      append += ' | x86_uops_retired_slots=' + x86_uops_retired_slots[1].split(',').join('');
+    }
+    // unfused domain, uops retired all
+    var x86_uops_retired_all = obj.stderr.toString().match(/([0-9,]+)\s+r1c2/m);
+    if (x86_uops_retired_all) {
+      append += ' | x86_uops_retired_all=' + x86_uops_retired_all[1].split(',').join('');
     }
     console.log(pad(bench, 15) + ' | ' + type + ' | time=' + elapsed_secs + append);
   } else {
@@ -66,7 +85,7 @@ function bench_native(benchmarks, target)
 {
   benchmarks.forEach(function(bench) {
      bench_cmd(bench, 'native-' + target, 'perf',
-       ['stat', '-e', 'instructions,r1c2', 'bin/' + target + '/' + bench]);
+       ['stat', '-e', 'instructions,r1b1,r10e,r2c2,r1c2', 'bin/' + target + '/' + bench]);
   });
 }
 
